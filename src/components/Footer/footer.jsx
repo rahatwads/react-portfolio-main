f8294a2bcab0afa9805f2a49ed742f91582bba6a
Shivani Rahatwad <rahatwad.s@northeastern.edu>
2025-03-09 23:10:41 -0400

    footer change

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,7 +1,7 @@
 
 
 import React from "react";
-import styles from "./Footer.module.css"; // Import CSS for styling
+import styles from "./footer.module.css"; // Import CSS for styling
 
 const Footer = () => {
   return (
