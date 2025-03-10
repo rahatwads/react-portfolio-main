ace3965ed66c3d2bdb9e20a0bfc7396b7d109393
Shivani Rahatwad <rahatwad.s@northeastern.edu>
2025-03-10 00:54:18 -0400

    Updated mobile responsiveness and styles

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-portfolio-main/",
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
-  },
 });
